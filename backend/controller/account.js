066670dde14e3d2506c3d2435f013d1cadccabf4
CarlJanoras <janorascarlxd@gmail.com>
2016-11-12 05:17:59 +0800

    added backend folder

---
+++ backend/controller/account.js
@@ -0,0 +1,81 @@
+'use strict'; 
+
+var db = require(__dirname + '/../lib/mysql');
+
+exports.create = function(req, res) {
+	var queryString = "INSERT INTO account"
+					+ "(email, password)"
+					+ "VALUES(?,?)";
+	db.query(
+		queryString,
+		[
+			req.body.email,
+			req.body.password
+		],
+		function(err, rows) {
+			if (err) {
+				return res.status(500).send({code: err.code});
+			}
+			res.send(rows);
+		}
+	);
+};
+
+exports.login = function(req, res, next) {
+	var data = {
+		email: req.body.email,
+		password: req.body.password
+	};
+	//what to do at the start of the query
+	function start() {
+		//Do not append the user inputs to string. This eliminates SQL Injection.
+		var queryString = "SELECT *"
+						+ " FROM account"
+						+ " WHERE email=?"
+						+ " AND password=?";
+		db.query(
+			queryString,
+			[
+				data.email, 
+				data.password
+			],
+			send_response
+		);
+	};
+
+	//what to do after the query is done
+	function send_response(err, result, args, last_query) {
+		if(err) {
+			next(err);
+			return res.status(500).send(err);
+		} else if(!result.length) {
+			//Do not send which one is wrong. This eliminates user enumeration.
+			return res.status(404).send({message: 'Wrong username or password.'});
+		} else {
+			req.session.user = {
+				username: result[0].username,
+			};
+			return res.send(result[0]);
+		}
+	};
+	start();
+};
+
+exports.logout = function(req, res, next) {
+	function start() {
+		req.session.destroy();
+		return res.send({message: 'Logout success!'});
+	};
+	start();
+};
+
+exports.checkSession = function (req, res, next) {
+    function  start() {
+		if (!req.session.username) {
+        	return res.send("NO_SESSION");
+    	} else {
+        	return res.send("SESSION");
+    	}
+	};
+	start();
+};
---
+++ backend/controller/job.js
@@ -0,0 +1,44 @@
+'use strict'; 
+
+var db = require(__dirname + '/../lib/mysql');
+
+exports.findAll = function(req, res) {
+	var queryString = "SELECT *"
+					+ "FROM job";
+	db.query(
+		queryString, 
+		function(err, rows) {
+			if (err) {
+				return res.status(500).send({code: err.code});
+			}
+			if (rows.length === 0) {
+				return res.status(404).send({error:true, message:'No jobs found.'});
+			}
+			res.send(rows);
+		} 
+	);
+};
+
+exports.search = function(req, res) {
+	var queryString = "SELECT *"
+					+ "FROM job"
+					+ "WHERE name=?"
+					+ "OR description=?"
+					+ "OR location=?";
+	db.query(
+		queryString, 
+		[
+			req.body.name, 
+			req.body.description
+		],
+		function(err, rows) {
+		if (err) {
+			return res.status(500).send({code: err.code});
+		}
+		if (rows.length === 0) {
+			return res.status(404).send({error:true, message:'No jobs found.'});
+		}
+		res.send(rows);
+	});
+};
+
