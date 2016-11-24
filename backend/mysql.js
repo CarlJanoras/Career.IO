fa22f5e1f358a0f5e6fac4d77c16d5722a8d41f4
CarlJanoras <janorascarlxd@gmail.com>
2016-11-25 05:02:33 +0800

    Update mysql.js

--- backend/mysql.js
+++ backend/mysql.js
@@ -1,6 +1,6 @@
 'use strict';
 
 var mysql = require('mysql');
-var config	= require(__dirname + '/config');
+var config	= require(__dirname + '/config/config');
 
 module.exports = mysql.createConnection(config.DATABASE_CREDENTIALS);
