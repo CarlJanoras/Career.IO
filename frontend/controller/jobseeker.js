2dcdf0b4e57ce4338dcb5220c0a14d4760b29a34
Ken Mercado <mightymercado@gmail.com>
2016-11-25 06:22:03 +0800

    Updated jobseeker.js

--- frontend/controller/jobseeker.js
+++ frontend/controller/jobseeker.js
@@ -1,39 +1,26 @@
 var jobseeker = angular.module("jobseeker", []);
 
+var getParam = function (key){
+	var result = new RegExp(key + "=([^&]*)", "i").exec(window.location.search);
+	return result && unescape(result[1]) || "";
+}
+
 jobseeker.controller("jobSeekerController", function($scope, $rootScope) {
 	$rootScope.location = "Jobs";
 	$scope.profile = {
-		name : "Kendrick Timothy B. Mercado",
-		location : "Los Banos, Philippines",
-		skills : ["Programming", "Designing", "Boxing"],
-		email: "mightymercado@gmail.com",
-		number: 1020939393,
-		description : "The quickest and brownest fox jumped over lazy dogs one at a time.",
-		education : [
-			{
-				school: "University of the Philippines",
-				attainment: "College",
-				start: "August 2012",
-				end: "November 2016",
-				description : "Quick Brown Fox"
-			},
-			{
-				school: "University of the Philippines",
-				attainment: "College",
-				start: "August 2012",
-				end: "November 2016",
-				description : "Quick Brown Fox"
-			}
-		],
-		work : [
-			{
-				title: "Editor-in-chief",
-				company : "Kalibrr",
-				description : "test",
-				start: "20101",
-				end: "2012"
-			}
-		]
+		name : "",
+		location : "",
+		skills : [],
+		email: "",
+		number: "",
+		description : "",
+		education : []
 	};
 
+	$http.get('/getJobSeekerDetails', {
+		params: { id: getParam('id') }
+	}).then(function (response) {
+		console.log(response.data[0]);
+		$scope.profile.address = response.data[0].address + ", " + response.data[0].city + ", " + response.data[0].country;
+	});
 });
