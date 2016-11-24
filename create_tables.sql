81ef5865f9df2d8985169951508275f3bb3e19cd
Ken Mercado <mightymercado@gmail.com>
2016-11-24 23:17:14 +0800

    New Proper Database

--- create_tables.sql
+++ create_tables.sql
@@ -1,257 +1,112 @@
--- MySQL dump 10.16  Distrib 10.1.18-MariaDB, for Linux (x86_64)
---
--- Host: localhost    Database: jobFinder
--- ------------------------------------------------------
--- Server version	10.1.18-MariaDB
+drop if exists jobFinder;
+create database jobFinder;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+create table JOB_SEEKER (
+ account_id     int auto_increment primary key not null,
+ email          varchar(256) not null,
+ password       varchar(256) not null,
+ last_login     date not null,
+ creation_date  date not null,
+ birthday       date not null,
+ age            date not null,
+ phone_number   int, 
+ sex            enum('male', 'female') not null,
+ first_name     varchar(256) not null,
+ middle_name    varchar(256) not null,
+ last_name      varchar(256) not null,
+ address        varchar(256),
+ city           varchar(256),
+ country        varchar(256)
+);
 
---
--- Table structure for table `account`
---
+create table EMPLOYER (
+ account_id          int auto_increment primary key not null,
+ email               varchar(256) not null,
+ password            varchar(256) not null,
+ last_login          date not null,
+ creation_date       date not null,
+ company_name        varchar(256) not null,
+ website             varchar(256),
+ industry            enum('Accountancy, banking and finance','Business, consulting and management','Creative arts and design','Energy and utilities','Engineering and manufacturing','Environment and agriculture','Healthcare','Hospitality and events management','Information technology','Law','Law enforcement and security','Leisure, sport and tourism','Marketing, advertising and PR','Property and construction','Public services and administration','Recruitment and HR','Retail','Sales','Science and pharmaceuticals','Teaching and education','Transport and logistics', 'Other') not null,
+ company_description varchar(2048),
+ address             varchar(256),
+ city                varchar(256),
+ country             varchar(256)
+);
 
-DROP TABLE IF EXISTS `account`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `account` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(256) NOT NULL,
-  `password` varchar(256) NOT NULL,
-  `last_login` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `creation_date` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+create table JOB (
+ job_id                 int auto_increment primary key not null,
+ job_title              varchar(256) not null,
+ job_description        varchar(2048),
+ employment_type        enum('Part-time', 'Full-time'),
+ job_level              enum('Internship / OJT','Fresh Grad / Entry Level','Associate / Supervisor','Mid-Senior Level / Manager', 'Director / Executive'),
+ salary                 int,
+ application_deadline   date,
+ industry               enum('Accountancy, banking and finance','Business, consulting and management','Creative arts and design','Energy and utilities','Engineering and manufacturing','Environment and agriculture','Healthcare','Hospitality and events management','Information technology','Law','Law enforcement and security','Leisure, sport and tourism','Marketing, advertising and PR','Property and construction','Public services and administration','Recruitment and HR','Retail','Sales','Science and pharmaceuticals','Teaching and education','Transport and logistics', 'Other') not null,
+ address                varchar(256),
+ city                   varchar(256),
+ country                varchar(256),
+ account_id             int not null,
+ Constraint            `fk_job_employer`
+  foreign key (account_id) REFERENCES EMPLOYER (account_id)
+  on delete cascade
+  on update restrict
+);
 
---
--- Dumping data for table `account`
---
+create table EDUCATION (
+ education_id       int auto_increment primary key not null,
+ description        varchar(2048),
+ school_name        varchar(256) not null,
+ finished           date,
+ start              date,
+ attainment         enum('Bachelor\'s Degree', 'High School', 'Vocational Course', 'Associate\'s Degree', 'Master\'s Degree', 'Doctorate'),
+ account_id         int not null,
+ Constraint         `fk_education_job_seeker`
+  foreign key (account_id) references JOB_SEEKER (account_id)
+  on delete cascade
+  on update restrict
+);
 
-LOCK TABLES `account` WRITE;
-/*!40000 ALTER TABLE `account` DISABLE KEYS */;
-/*!40000 ALTER TABLE `account` ENABLE KEYS */;
-UNLOCK TABLES;
+create table REQUIREMENT (
+ requirement_id            int auto_increment primary key not null,
+ educational_attainment    varchar(256) not null,
+ job_id                    int not null,
+ Constraint               `fk_requirement_job`
+  foreign key (job_id) references JOB (job_id)
+  on delete cascade
+  on update restrict
+);
 
---
--- Table structure for table `application`
---
+create table REQUIREMENT_SKILL (
+ requirement_id        int not null,
+ skill_name            varchar(128) not null,
+ PRIMARY KEY           (requirement_id, skill_name)
+);
 
-DROP TABLE IF EXISTS `application`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `application` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `job_seeker_id` int(11) NOT NULL,
-  `employer_id` int(11) NOT NULL,
-  `job_id` int(11) NOT NULL,
-  `date_applied` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_application_jobseeker` (`job_seeker_id`),
-  KEY `fk_application_employer` (`employer_id`),
-  KEY `fk_application_job` (`job_id`),
-  CONSTRAINT `fk_application_employer` FOREIGN KEY (`employer_id`) REFERENCES `account` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `fk_application_job` FOREIGN KEY (`job_id`) REFERENCES `job` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `fk_application_jobseeker` FOREIGN KEY (`job_seeker_id`) REFERENCES `account` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+create table WORK_EXPERIENCE (
+ work_id          int auto_increment primary key not null,
+ job_title        varchar(256) not null,
+ employer         varchar(256) not null,
+ start            date,
+ finished         date,
+ description      varchar(2048),
+ account_id       int not null,
+ Constraint       `fk_workexperience_job_seeker`
+  foreign key (account_id) REFERENCES JOB_SEEKER (account_id)
+  on delete cascade
+  on update restrict
+);
 
---
--- Dumping data for table `application`
---
+create table APPLIES (
+ account_id        int not null,
+ job_id            int not null,
+ date_applied      date not null,
+ PRIMARY KEY       (account_id, job_id, date_applied)
+);
 
-LOCK TABLES `application` WRITE;
-/*!40000 ALTER TABLE `application` DISABLE KEYS */;
-/*!40000 ALTER TABLE `application` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `education`
---
-
-DROP TABLE IF EXISTS `education`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `education` (
-  `account_id` int(11) NOT NULL,
-  `attainment` enum('Bachelor''s Degree','High School','Vocational Course','Associate''s Degree','Master''s Degree','Doctorate') DEFAULT NULL,
-  `graduated` tinyint(1) NOT NULL DEFAULT '0',
-  `start` date DEFAULT NULL,
-  `finished` date DEFAULT NULL,
-  `description` varchar(2048) DEFAULT NULL,
-  `school` varchar(256) NOT NULL,
-  KEY `fk_education_employee` (`account_id`),
-  CONSTRAINT `fk_education_employee` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `education`
---
-
-LOCK TABLES `education` WRITE;
-/*!40000 ALTER TABLE `education` DISABLE KEYS */;
-/*!40000 ALTER TABLE `education` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `employer`
---
-
-DROP TABLE IF EXISTS `employer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `employer` (
-  `account_id` int(11) NOT NULL,
-  `name` varchar(256) NOT NULL,
-  `website` varchar(256) DEFAULT NULL,
-  `industry` enum('Accountancy, banking and finance','Business, consulting and management','Creative arts and design','Energy and utilities','Engineering and manufacturing','Environment and agriculture','Healthcare','Hospitality and events management','Information technology','Law','Law enforcement and security','Leisure, sport and tourism','Marketing, advertising and PR','Property and construction','Public services and administration','Recruitment and HR','Retail','Sales','Science and pharmaceuticals','Teaching and education','Transport and logistics','Other') NOT NULL,
-  `description` varchar(2048) DEFAULT NULL,
-  `address` varchar(256) DEFAULT NULL,
-  `city` varchar(256) DEFAULT NULL,
-  `country` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`account_id`),
-  CONSTRAINT `fk_employer_account` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `employer`
---
-
-LOCK TABLES `employer` WRITE;
-/*!40000 ALTER TABLE `employer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `employer` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `job`
---
-
-DROP TABLE IF EXISTS `job`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `job` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(256) NOT NULL,
-  `description` varchar(2048) DEFAULT NULL,
-  `employment_level` enum('Part-time','Full-time') DEFAULT NULL,
-  `job_level` enum('Internship / OJT','Fresh Grad / Entry Level','Associate / Supervisor','Mid-Senior Level / Manager','Director / Executive') DEFAULT NULL,
-  `salary` int(11) DEFAULT NULL,
-  `hiring` tinyint(1) NOT NULL DEFAULT '0',
-  `employer_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_job_employee` (`employer_id`),
-  CONSTRAINT `fk_job_employee` FOREIGN KEY (`employer_id`) REFERENCES `account` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `job`
---
-
-LOCK TABLES `job` WRITE;
-/*!40000 ALTER TABLE `job` DISABLE KEYS */;
-/*!40000 ALTER TABLE `job` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `job_seeker`
---
-
-DROP TABLE IF EXISTS `job_seeker`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `job_seeker` (
-  `account_id` int(11) NOT NULL,
-  `first_name` varchar(256) NOT NULL,
-  `middle_name` varchar(256) NOT NULL,
-  `last_name` varchar(256) NOT NULL,
-  `address` varchar(256) DEFAULT NULL,
-  `city` varchar(256) DEFAULT NULL,
-  `country` varchar(256) DEFAULT NULL,
-  `phone` int(11) DEFAULT NULL,
-  PRIMARY KEY (`account_id`),
-  CONSTRAINT `fk_jobseeker_account` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `job_seeker`
---
-
-LOCK TABLES `job_seeker` WRITE;
-/*!40000 ALTER TABLE `job_seeker` DISABLE KEYS */;
-/*!40000 ALTER TABLE `job_seeker` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `skill`
---
-
-DROP TABLE IF EXISTS `skill`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `skill` (
-  `account_id` int(11) NOT NULL,
-  `skill` varchar(256) NOT NULL,
-  KEY `fk_skill_jobseeker` (`account_id`),
-  CONSTRAINT `fk_skill_jobseeker` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `skill`
---
-
-LOCK TABLES `skill` WRITE;
-/*!40000 ALTER TABLE `skill` DISABLE KEYS */;
-/*!40000 ALTER TABLE `skill` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `work_experience`
---
-
-DROP TABLE IF EXISTS `work_experience`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `work_experience` (
-  `account_id` int(11) NOT NULL,
-  `start` date DEFAULT NULL,
-  `finished` date DEFAULT NULL,
-  `description` varchar(2048) DEFAULT NULL,
-  `job_title` varchar(256) NOT NULL,
-  `employer` varchar(256) NOT NULL,
-  KEY `fk_workexperience_employee` (`account_id`),
-  CONSTRAINT `fk_workexperience_employee` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `work_experience`
---
-
-LOCK TABLES `work_experience` WRITE;
-/*!40000 ALTER TABLE `work_experience` DISABLE KEYS */;
-/*!40000 ALTER TABLE `work_experience` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-11-12  4:59:11
+create table POSTS (
+ account_id        int not null,
+ job_id            int not null,
+ date_posted       date not null,
+ PRIMARY KEY       (account_id, job_id, date_posted)
+);
