641e423ae8e79ef9ad7e12ca820bef6926d93904
CarlJanoras <janorascarlxd@gmail.com>
2016-11-25 07:19:12 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 'use strict';
 
-var config = require(__dirname + '/config');
+var config = require(__dirname + '/config/config');
 var express = require('express'),
 	session = require('express-session'),
 	bodyParser = require('body-parser'),
+    	methodOverride = require('method-override'),
+	router = require(__dirname + '/config/router'),
 	app = express(),
 
-	server = app.listen(config.PORT, config.IP,
-    	function(){
-	        var host = server.address().address;
-	        var port = server.address().port;
-	        console.log('Example app is listening at http://%s:%s', host, port);
-    	});
+	server = app.listen(config.PORT, 
+			    config.IP, 
+			    function(){
+					var host = server.address().address;
+					var port = server.address().port;
+					console.log('Example app is listening at http://%s:%s', host, port);
+					}
+			   );
 
 app.use(express.static(config.ASSETS_DIR));
 
@@ -20,7 +24,7 @@ console.log('info:', 'Setting up sessions...');
 app.use(
 	session({
 	    secret: config.COOKIE_SECRET,
-	    name: 'CMSC127',
+	    name: 'cMsC_127',
 	    resave: true,
 	    rolling: true,
 	    saveUninitialized: true,
@@ -29,11 +33,11 @@ app.use(
 	    }
  	})
 );
-
+app.use(methodOverride());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
 app.use(bodyParser.json());
 
-app.use(require(__dirname + '/router')(express.Router()));
+app.use(router(express.Router()));
