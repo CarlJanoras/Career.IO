a32655e66544a90fdb48784721210747dbee0e60
CarlJanoras <janorascarlxd@gmail.com>
2016-11-25 14:28:10 +0800

    Update applies.js

--- backend/controller/applies.js
+++ backend/controller/applies.js
@@ -1,1 +1,22 @@
 
+'use strict'; 
+
+var db = require(__dirname + '/../mysql');
+
+exports.addApplication = function(res, req) {
+      var query = "insert into APPLIES "
+            + "values (?, ?, CURDATE())";
+      db.query(query,
+              [
+                req.body.account_id,
+                req.body.job_id
+              ],
+              function(err, rows) {
+                if (err) {
+                  return res.status(500).send({code: err.code});
+                }
+                res.send(rows);
+              }
+      );
+  
+};
