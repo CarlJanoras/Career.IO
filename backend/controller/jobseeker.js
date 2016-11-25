a2efeff96c874a83a3e5574c704d8a5031fad8b6
CarlJanoras <janorascarlxd@gmail.com>
2016-11-25 10:19:56 +0800

    Update jobseeker.js

--- backend/controller/jobseeker.js
+++ backend/controller/jobseeker.js
@@ -2,14 +2,18 @@
 
 var db = require(__dirname + '/../mysql');
 
-exports.add = function(req, res) {
-	var query = "insert into JOB_SEEKER"
-		+ "(email, password, last_login, "
-    		+ "creation_date, birthday, "
-    		+ "phone_number, sex, first_name, "
-    		+ "middle_name, last_name, address, "
-    		+ "city, country) "
-    		+ "values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+exports.addJobseeker = function(req, res) {
+	var query = "insert into" 
+		+ "	JOB_SEEKER ("	
+		+ "		email, password, last_login, "
+		+ "		creation_date, first_name, middle_name,"
+		+ "		last_name, birthday, sex "
+		+ "	) "
+		+ "values ( "
+		+ "	?, ?, CURDATE(), "
+		+ "	CURDATE(), ?, ?, "
+		+ "	?, ?, ? "
+		+ ")";
 	
   	db.query(query,
 		[
@@ -17,29 +21,56 @@ exports.add = function(req, res) {
 			req.body.password,
 			req.body.last_login,
 			req.body.creation_date,
-			req.body.birthday,
-			req.body.phone_number,
-			req.body.sex,
 			req.body.first_name,
 			req.body.middle_name,
 			req.body.last_name,
-      			req.body.address,
-      			req.body.city,
-      			req.body.country
+			req.body.birthday,
+			req.body.sex
 		],
 		function(err, rows) {
 			if (err) {
 				return res.status(500).send({code: err.code});
 			}
 			res.send(rows);
-		});
+		}
+	);
 };
 
-exports.viewProfile = function(res, req) {
-	var query = "select * from JOB_SEEKER"
-		+ "where account_id=?";
-	
-	db.query(query, 
+exports.getJobSeeker = function(res, req) {
+	var queryJobSeeker = "select "
+		+ "	email, description, "
+		+ "	birthday, phone_number, sex, "
+		+ "	first_name, middle_name, last_name, "
+		+ "	address, city, country "
+		+ "from "
+		+ "	JOB_SEEKER "
+		+ "where "
+		+ "	account_id = ?";
+	var queryJobSeekerSkill = "select 
+				+ "	skill 
+				+ "from 
+				+ "	JOB_SEEKER_SKILL 
+				+ "where 
+				+ "	account_id = ?";
+	var queryJobSeekerWorkExp = "select "
+				+ "	job_title, employer, start, "
+				+ "	finished, description "
+				+ "from "
+				+ "	WORK_EXPERIENCE "
+				+ "where "
+				+ "	account_id = ?";
+	var queryJobSeekerEduc = "select "
+				+ "	description, school_name, "
+				+ "	finished, start, attainment "
+				+ "from "
+				+ "	EDUCATION "
+				+ "where "
+				+ "	account_id = ?";
+	var job_seeker_row;
+	var job_seeker_skills;
+	var job_seeker_experiences;
+	var job_seeker_educations;
+	db.query(queryJobSeeker, 
 		[
 			req.body.account_id
 		],
@@ -47,6 +78,47 @@ exports.viewProfile = function(res, req) {
 			if (err) {
 				return res.status(500).send({code: err.code});
 			}
-			res.send(rows);
-		});
+			job_seeker_row = rows[0];
+		}
+	);
+	
+	db.query(queryJobSeekerSkill,
+		[
+			req.body.account_id
+		],
+		function(err, rows) {
+			if (err) {
+				return res.status(500).send({code: err.code});
+			}
+			job_seeker_skills = rows;
+			job_seeker_row.skill = job_seeker_skills;
+		}
+	);
+	
+	db.query(queryJobSeekerWorkExp,
+		[
+			req.body.account_id
+		],
+		function(err, rows) {
+			if (err) {
+				return res.status(500).send({code: err.code});
+			}
+			job_seeker_experiences = rows;
+			job_seeker_row.work_exp = job_seeker_experiences;
+		}
+	);
+	
+	db.query(queryJobSeekerEduc,
+		[
+			req.body.account_id
+		],
+		function(err, rows) {
+			if (err) {
+				return res.status(500).send({code: err.code});
+			}
+			job_seeker_educations = rows;
+			job_seeker_row.educations = job_seeker_educations;
+		}
+	);
+	res.send(job_seeker_row);
 };
