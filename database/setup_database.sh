84ea967de6eb79ce303fd931cd9c999b22ec580c
Ken Mercado <mightymercado@gmail.com>
2016-11-30 18:49:09 +0800

    Updated script to insert procedures

--- database/setup_database.sh
+++ database/setup_database.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-mysql -uroot < create_tables.sql && echo "Created Tables" && 
-mysql -uroot < insert_job_seekers.sql && echo "Inserted Job Seekers Data" && 
-mysql -uroot < insert_employers.sql && echo "Inserted Employers Data" && 
-mysql -uroot < insert_jobs.sql && echo "Inserted Jobs Data" && 
-mysql -uroot < insert_applies.sql && echo "Inserted Applies Data"
+mysql -uroot < create_tables.sql            && echo "Created Tables"                  && 
+mysql -uroot < insert_job_seekers.sql       && echo "Inserted Job Seekers Data"       && 
+mysql -uroot < insert_employers.sql         && echo "Inserted Employers Data"         && 
+mysql -uroot < insert_jobs.sql              && echo "Inserted Jobs Data"              && 
+mysql -uroot < insert_applications.sql      && echo "Inserted Application Data"       &&
+mysql -uroot < jobseeker_procedures.sql     && echo "Inserted Job Seekers Procedures" && 
+mysql -uroot < employer_procedures.sql      && echo "Inserted Employers Procedures"   && 
+mysql -uroot < job_procedures.sql           && echo "Inserted Jobs Procedures"        && 
+mysql -uroot < application_procedures.sql   && echo "Inserted Application Procedures" &&     
