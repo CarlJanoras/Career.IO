8c38a8d8da3cb451cfd2c9ca3bbcd705def7353b
CarlJanoras <janorascarlxd@gmail.com>
2016-11-30 17:47:39 +0800

    Update and rename insert_applies.sql to insert_application.sql

---
+++ database/insert_application.sql
@@ -0,0 +1,12 @@
+use jobFinder;
+delete from APPLICATION;
+insert into APPLICATION values (6, 3, "2016-09-17");
+insert into APPLICATION values (8, 7, "2016-10-14");
+insert into APPLICATION values (2, 3, "2016-05-12");
+insert into APPLICATION values (2, 8, "2016-01-27");
+insert into APPLICATION values (2, 8, "2016-02-17");
+insert into APPLICATION values (9, 10, "2016-03-20");
+insert into APPLICATION values (7, 2, "2016-03-16");
+insert into APPLICATION values (1, 8, "2016-03-20");
+insert into APPLICATION values (10, 7, "2016-04-24");
+insert into APPLICATION values (2, 10, "2016-04-16");
